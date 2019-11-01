0039c16aca03d21fbb6e873aa8c2c7cbe8a7a294
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-10-31 22:01:12 -0500

    Make it so when you click on the grid, it adds a block

--- src/js/CanvasController.js
+++ src/js/CanvasController.js
@@ -1,8 +1,8 @@
 let spriteWidthPixels;
 let spriteHeightPixels;
 
-let spriteWidth = 30;
-let spriteHeight = 20;
+let spriteWidth = 12;
+let spriteHeight = 10;
 
 function setupCanvas() {
     let canvas = document.getElementById("mainCanvas");
@@ -10,6 +10,7 @@ function setupCanvas() {
 
     spriteWidthPixels = canvas.width / spriteWidth;
     spriteHeightPixels = canvas.height / spriteHeight;
+
     for (let i = 0; i < spriteWidth; i++) {
         for (let j = 0; j < spriteHeight; j++) {
             if (j % 2 == 0) {
@@ -23,24 +24,29 @@ function setupCanvas() {
    
             context.fill(); 
         }
+
+        canvas.addEventListener('mouseup', function(evt) {
+            var mousePos = getMousePos(canvas, evt);
+            drawPoint(Math.floor(mousePos.x / spriteWidthPixels), Math.floor(mousePos.y / spriteHeightPixels));
+          }, false);
     }
-    
-    //context.fillStyle = "#000000";
-    //context.rect(10, 10, spriteWidthPixels, spriteHeightPixels); 
-    context.fill(); 
 }
 function drawPoint(x, y) {
     let canvas = document.getElementById("mainCanvas");
     let context = document.getElementById("mainCanvas").getContext("2d");
+    context.beginPath();
     context.fillStyle = "#ffc821";
-    context.rect(x * 15,y * 15,canvas.width / spriteWidth,canvas.height / spriteHeight);       
+    context.rect(x * spriteWidthPixels, y * spriteHeightPixels, canvas.width / spriteWidth,canvas.height / spriteHeight);       
     context.fill();
 }
 
 function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
+    var rect = canvas.getBoundingClientRect(),
+    scaleX = canvas.width / rect.width,
+    scaleY = canvas.height / rect.height;
+
     return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
-    };
+        x: (evt.clientX - rect.left) * scaleX,
+        y: (evt.clientY - rect.top) * scaleY
+    }
   }
