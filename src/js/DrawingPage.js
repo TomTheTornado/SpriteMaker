1348830d99bb84aef5168a468b086afd344445cc
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-11-07 23:23:57 -0600

    Fix background so it isn't added to the export

--- src/js/DrawingPage.js
+++ src/js/DrawingPage.js
@@ -1,10 +1,5 @@
 
-function exportCanvas() {
-    let canvas = document.getElementById("mainCanvas");
-    let img = canvas.toDataURL("image/png");
 
-    document.write('<img src="'+img+'"/>');
-}
 
 function saveCanvas() {
     // TODO
