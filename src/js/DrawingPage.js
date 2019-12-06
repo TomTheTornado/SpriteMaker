9ccbeae0e4bffdbb82695dcbabd9726c163ab537
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-12-06 09:33:55 -0600

    SPRITE-15 add layer buttons and change styling slightly

--- src/js/DrawingPage.js
+++ src/js/DrawingPage.js
@@ -1,5 +1,12 @@
 
+function selectButton(element) {
+    document.getElementById('layer1button').className="btn btn-dark";
+    document.getElementById('layer2button').className="btn btn-dark";
+    document.getElementById('layer3button').className="btn btn-dark";
+    document.getElementById(element).className = "btn btn-light";
 
+
+}
 
 function saveCanvas() {
     // TODO
